b4429409cb9edcb8fb5e52de7ff3a4297a5f75d8
arnaud cormier <arno.cormier@gmail.com>
2019-06-03 22:26:27 +0200

    init new mix project with a supervision tree

---
+++ lib/elixirwithecto.ex
@@ -0,0 +1,18 @@
+defmodule Elixirwithecto do
+  @moduledoc """
+  Documentation for Elixirwithecto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirwithecto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirwithecto_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirwithectoTest do
+  use ExUnit.Case
+  doctest Elixirwithecto
+
+  test "greets the world" do
+    assert Elixirwithecto.hello() == :world
+  end
+end
