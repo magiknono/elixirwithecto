287d17e6c002dce910dce8e4bcaea05f4d03b399
arnaud cormier <arno.cormier@gmail.com>
2019-06-04 01:12:45 +0200

    add memo when starting : iex -S mix

---
+++ .iex.exs
@@ -0,0 +1,11 @@
+IO.puts "== Learn ecto with elixir =="
+IO.puts " - https://elixir-lang.org/"
+IO.puts " - https://elixirschool.com/en/lessons/ecto/basics/"
+IO.puts " - https://hexdocs.pm/ecto/Ecto.html"
+IO.puts "== noob memo : == "
+IO.puts "h ecto"
+IO.puts "exports(Ecto. )"
+IO.puts "use alias and import Ecto.Query"
+IO.puts "Starting iex ..."
+
+
