9420a272190192661ff010e9f7093371e8202216
arnaud cormier <arno.cormier@gmail.com>
2019-06-04 13:35:01 +0200

    add create and list actions with ecto

--- lib/elixirwithecto/language.ex
+++ lib/elixirwithecto/language.ex
@@ -1,5 +1,7 @@
 defmodule Elixirwithecto.Language do
   use Ecto.Schema
+  alias Elixirwithecto.Repo
+  alias Elixirwithecto.Language
 
   schema "language" do
     field :name, :string, null: false
@@ -9,5 +11,14 @@ defmodule Elixirwithecto.Language do
     field :active, :boolean, default: true
 
     timestamps()
+  end
+
+  def languages do
+    Repo.all Language
+  end
+
+  def create_language(name, first_version, paradigm, actual_version) do
+    %Language{name: name, first_version: first_version, paradigm: paradigm, actual_version: actual_version}
+    |> Repo.insert()
   end
 end
